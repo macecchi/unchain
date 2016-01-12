1a2851d828982aabd319107ba7d7cb35aa97c421
Mario Cecchi <macecchi@gmail.com>
2016-01-11 22:57:13 -0200

    UI with images and loading bar

--- src/unchain.c
+++ src/unchain.c
@@ -1,5 +1,9 @@
 #include <pebble.h>
+#include "progress_layer.h"
 #include "animations.h"
+
+#define PROGRESS_LAYER_WINDOW_DELTA 33
+#define PROGRESS_LAYER_WINDOW_WIDTH 80
 
 enum {
 	APP_KEY_ACTION = 0,
@@ -13,9 +17,19 @@ typedef enum {
     MAC_STATE_UNLOCKED
 } MacState;
 
+
+static void progress_callback(void *context);
+
 static MacState macState = MAC_STATE_UNKNOWN;
 static Window *window;
 static TextLayer *text_layer;
+static GBitmap *locked_bitmap;
+static GBitmap *unlocked_bitmap;
+static BitmapLayer *locked_bitmap_layer;
+static BitmapLayer *unlocked_bitmap_layer;
+static ProgressLayer *s_progress_layer;
+static AppTimer *s_timer;
+static int s_progress;
 
 
 // Message handling
@@ -40,17 +54,36 @@ static void in_received_handler(DictionaryIterator *iter, void *context) {
 
 	Tuple *state = dict_find(iter, APP_KEY_STATE);
 	if (state) {
+	   Layer *window_layer = window_get_root_layer(window);
+       layer_remove_from_parent(s_progress_layer);
 		if (strcmp(state->value->cstring, "unlocked") == 0) {
 			APP_LOG(APP_LOG_LEVEL_DEBUG, "Mac is unlocked.");
             update_text_with_animation(text_layer, "UNLOCKED");
+            layer_remove_from_parent(bitmap_layer_get_layer(locked_bitmap_layer));
+            layer_add_child(window_layer, bitmap_layer_get_layer(unlocked_bitmap_layer));
             macState = MAC_STATE_UNLOCKED;
 		}
         else if (strcmp(state->value->cstring, "locked") == 0) {
 			APP_LOG(APP_LOG_LEVEL_DEBUG, "Mac is locked.");
             update_text_with_animation(text_layer, "LOCKED");
+            layer_remove_from_parent(bitmap_layer_get_layer(unlocked_bitmap_layer));
+            layer_add_child(window_layer, bitmap_layer_get_layer(locked_bitmap_layer));
             macState = MAC_STATE_LOCKED;
 		}
 	}
+}
+
+
+// Progress bar handling
+
+static void next_timer() {
+  s_timer = app_timer_register(PROGRESS_LAYER_WINDOW_DELTA, progress_callback, NULL);
+}
+
+static void progress_callback(void *context) {
+  s_progress += (s_progress < 100) ? 1 : -100;
+  progress_layer_set_progress(s_progress_layer, s_progress);
+  next_timer();
 }
 
 
@@ -83,9 +116,27 @@ static void click_config_provider(void *context) {
 static void window_load(Window *window) {
 	Layer *window_layer = window_get_root_layer(window);
 	GRect bounds = layer_get_bounds(window_layer);
+    
+    locked_bitmap = gbitmap_create_with_resource(RESOURCE_ID_LOCKED_IMAGE);
+    unlocked_bitmap = gbitmap_create_with_resource(RESOURCE_ID_UNLOCKED_IMAGE);
+    
+    s_progress_layer = progress_layer_create(GRect((bounds.size.w - PROGRESS_LAYER_WINDOW_WIDTH) / 2, 80, PROGRESS_LAYER_WINDOW_WIDTH, 6));
+    progress_layer_set_progress(s_progress_layer, 0);
+    progress_layer_set_corner_radius(s_progress_layer, 2);
+    progress_layer_set_foreground_color(s_progress_layer, GColorWhite);
+    progress_layer_set_background_color(s_progress_layer, GColorBlack);
+    layer_add_child(window_layer, s_progress_layer);
 
-	text_layer = text_layer_create((GRect) { .origin = { 0, 115 }, .size = { bounds.size.w, 35 } });
-	text_layer_set_text(text_layer, "Connecting...");
+    GRect image_rect = GRect(bounds.size.w/2 - 49/2, 35, 49, 68);
+    locked_bitmap_layer = bitmap_layer_create(image_rect);
+    bitmap_layer_set_bitmap(locked_bitmap_layer, locked_bitmap);
+    bitmap_layer_set_compositing_mode(locked_bitmap_layer, GCompOpSet);
+    unlocked_bitmap_layer = bitmap_layer_create(image_rect);
+    bitmap_layer_set_bitmap(unlocked_bitmap_layer, unlocked_bitmap);
+    bitmap_layer_set_compositing_mode(unlocked_bitmap_layer, GCompOpSet);
+
+	text_layer = text_layer_create((GRect) { .origin = { 0, 110 }, .size = { bounds.size.w, 35 } });
+	text_layer_set_text(text_layer, "Finding Mac...");
 	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
 	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
     text_layer_set_background_color(text_layer, GColorClear);
@@ -94,6 +145,23 @@ static void window_load(Window *window) {
 
 static void window_unload(Window *window) {
 	text_layer_destroy(text_layer);
+    gbitmap_destroy(locked_bitmap);
+    gbitmap_destroy(unlocked_bitmap);
+    bitmap_layer_destroy(locked_bitmap_layer);
+    bitmap_layer_destroy(unlocked_bitmap_layer);
+    progress_layer_destroy(s_progress_layer);
+}
+
+static void window_appear(Window *window) {
+  s_progress = 0;
+  next_timer();
+}
+
+static void window_disappear(Window *window) {
+  if(s_timer) {
+    app_timer_cancel(s_timer);
+    s_timer = NULL;
+  }
 }
 
 
@@ -105,7 +173,9 @@ static void init(void) {
 	window_set_click_config_provider(window, click_config_provider);
 	window_set_window_handlers(window, (WindowHandlers) {
 		.load = window_load,
-		.unload = window_unload,
+        .appear = window_appear,
+        .disappear = window_disappear,
+		.unload = window_unload
 	});
 	const bool animated = true;
 	window_stack_push(window, animated);
