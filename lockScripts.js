40e17e0b3f20a1c26891cb3ff83b92726051e684
Mario Cecchi <macecchi@gmail.com>
2016-01-20 02:45:35 -0200

    Refactor lock scripts code style

--- lockScripts.js
+++ lockScripts.js
@@ -1,30 +1,32 @@
 'use strict';
-var applescript = require( 'applescript' );
-var exec = require( 'child_process' ).exec;
+var applescript = require('applescript');
+var exec = require('child_process').exec;
 
 module.exports = {
-    lock: function ( res, callback ) {
+    lock: function(res, callback) {
         var lockScript = 'do shell script "./lockscreen"';
 
-        applescript.execString( lockScript , function ( err, rtn ) {
-            callback( res, err, rtn );
+        applescript.execString(lockScript, function(err, rtn) {
+            callback(res, err, rtn);
         });
     },
 
-    isScreenlocked: function ( callback ) {
+    isScreenlocked: function(callback) {
         // easier test for screen lock based on https://gist.github.com/OmgImAlexis/1519cc5fa1ac392fb2d1
         // more info on using CGSessionCopyCurrentDictionary http://stackoverflow.com/questions/11505255/osx-check-if-the-screen-is-locked
         // but this seems sufficient so far
-        exec( 'python -c \'import sys,Quartz; d=Quartz.CGSessionCopyCurrentDictionary(); print d.get("CGSSessionScreenIsLocked")\'', function ( error, stdout, stderr ) {
+        // Thanks to https://github.com/suhajdab/lan-unlock
+        exec('python -c \'import sys,Quartz; d=Quartz.CGSessionCopyCurrentDictionary(); print d.get("CGSSessionScreenIsLocked")\'', function(error, stdout, stderr) {
             // response = True / None
-            if ( error ) console.error( error );
+            if (error) console.error(error);
 
-            if ( stdout.trim() == 'True' ) callback( true );
-            else callback( false );
+            if (stdout.trim() == 'True') callback(true);
+            else callback(false);
         });
     },
 
-    unlock: function ( pw, res, callback  ) {
+    unlock: function(password, res, callback) {
+        // Thanks to https://github.com/suhajdab/lan-unlock
         var unlockScript =
             'tell application "System Events"\n\
                 if name of every process contains "ScreenSaverEngine" then \n\
@@ -42,16 +44,14 @@ module.exports = {
                     activate\n\
                     delay 0.2\n\
                     tell window "Login Panel"\n\
-                        keystroke "password"\n\
+                        keystroke "' + password + '"\n\
                         keystroke return\n\
                     end tell\n\
                 end tell\n\
             end tell';
         
-        unlockScript = unlockScript.replace( 'password', pw );
-        
-        applescript.execString( unlockScript, function ( err, rtn ) {
-            callback( res, err, rtn );
+        applescript.execString(unlockScript, function(err, rtn) {
+            callback(res, err, rtn);
         });
     }
 };
